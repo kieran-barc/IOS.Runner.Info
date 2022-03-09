6b714b202f1068b4030329d2694fb3a742b99e22
Kieran Barclay <kieran.barclay22@capefearacademy.org>
2022-03-09 12:10:35 -0500

    Initial Commit

---
+++ IOS.Runner.Info/CollectionViewCell.swift
@@ -0,0 +1,16 @@
+//
+//  CollectionViewCell.swift
+//  IOS.Runner.Info
+//
+//  Created by robotics.class on 2/9/22.
+//
+
+import UIKit
+
+class CollectionViewCell: UICollectionViewCell {
+    
+    
+    @IBOutlet weak var cellImageView: UIImageView!
+    
+    
+}
